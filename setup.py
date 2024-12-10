fac68ebb3c2f0ea5ad3912a9cabee958758de03a
Asghari <mhmdasghari1@gmail.com>
2024-12-10 19:41:01 +0330

    fix: hope to include responses

--- setup.py
+++ setup.py
@@ -8,9 +8,11 @@ setup(
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/basalam/backbone-api",
-    packages=find_namespace_packages(where='src'),
+    packages=find_namespace_packages(where='src',
+                                     include=['basalam.backbone_api',
+                                              'basalam.backbone_api.responses',
+                                              'basalam.backbone_api.responses.*']),
     package_dir={'': 'src'},
-    namespace_packages=["basalam"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
@@ -19,7 +21,7 @@ setup(
     python_requires='>=3.6',
     setuptools_git_versioning={"enabled": True},
     setup_requires=["setuptools-git-versioning"],
-    isntall_requires=[
+    install_requires=[
         "pydantic>=2.9.1",
         "fastapi>=0.114.0"
     ]
---
+++ src/basalam/__init__.py
@@ -0,0 +1,6 @@
+# src/basalam/__init__.py
+
+try:
+    __import__('pkg_resources').declare_namespace(__name__)
+except ImportError:
+    pass
